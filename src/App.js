a1e6be7a65762f529ef0c5d2fcf3ad8c867e7837
kananmhn <kannanfrlearn@gmail.com>
2024-04-01 04:44:52 +0530

    download button added

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ import Home from "./pages/Home";
 function App() {
   return (
     <div className="App">
-      <Header />
+    
     <BrowserRouter>
+    <Header />
       <Routes>
           <Route path="/home" element={<Home />} />
           <Route path="/" element={<Layout />}>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./Header.css";
 
 const Header = () => {
+    const location = useLocation();
 
+    const handleDownload = async () => {
+        const response = await fetch('http://206.189.135.207:3001/api/get-colors');
+        const data = await response.json();
+      
+        // Remove the color_id field from each item in the data array
+        data.forEach(item => {
+          delete item.color_id;
+        });
+      
+        const json = JSON.stringify(data);
+        const blob = new Blob([json],{type:'application/json'});
+        const href = URL.createObjectURL(blob);
+        const link = document.createElement('a');
+        link.href = href;
+        link.download = 'theme-builder.json';
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+      }
+    
     return (
     
             <header className='header'>
@@ -13,9 +36,12 @@ const Header = () => {
                             <h1>Theme Builder</h1> 
                         </a>
                     </div>
-                    <div>
-                       
-                    </div>
+                    <div className='downloadJson'>
+                    {location.pathname === '/home' && (
+                        <button className="apply-color" onClick={handleDownload}>Download</button>
+                    )}
+                     </div>
+                
                 </div>
             </header>
         );
