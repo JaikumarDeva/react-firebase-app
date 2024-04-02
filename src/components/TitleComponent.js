f6dc287238f9380283f555e1f966b094e8e6e38e
kananmhn <159273393+kananmhn@users.noreply.github.com>
2024-04-02 22:20:36 +0530

    Add files via upload

---
+++ src/components/TitleComponent.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect, useRef } from 'react';
+import { SketchPicker } from 'react-color';
+
+function TitleComponent({sendDataToParent,importdata}) {
+  const [titleColor, setTitleColor] = useState("#FFFFFF");
+    const [displayColorPicker, setDisplayColorPicker] = useState(false);
+    const popover = useRef();
+
+    const handleTitleColorChange = (color) => {
+        setTitleColor(color.hex);
+      sendDataToParent(color.hex);
+    };
+
+    const handleClick = () => {
+      setDisplayColorPicker(true);
+    };
+    useEffect(() => {
+        if(importdata) {
+            setTitleColor(importdata);
+        }
+      },[importdata]) 
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (popover.current && !popover.current.contains(event.target)) {
+                setDisplayColorPicker(false);
+            }
+        }
+
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, []);
+
+    return (
+        <form>
+        <label>
+        <span className='lable-text'>Title color:</span>
+        <input type="text" value={titleColor} onClick={handleClick} readOnly />
+        { displayColorPicker ? <div className="popover" ref={popover}>
+            <SketchPicker color={titleColor} onChangeComplete={handleTitleColorChange} />
+        </div> : null }
+        </label>
+        </form>
+    );
+}
+
+export default TitleComponent;
