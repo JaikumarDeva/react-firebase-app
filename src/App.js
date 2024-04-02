a1b105d1f9fbb598e0f43008a00c3686e1e903b8
kananmhn <159273393+kananmhn@users.noreply.github.com>
2024-04-02 22:21:37 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./components/Header/Header";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./pages/Layout";
 import Home from "./pages/Home";
 
 function App() {
+  const [importData, setimportData] = useState("");
+  const handleDataFromChildCMP = (data) => {
+    setimportData(data);
+  }
   return (
     <div className="App">
-    
+
     <BrowserRouter>
     <Header />
-      <Routes>
-          <Route path="/home" element={<Home />} />
-          <Route path="/" element={<Layout />}>
+          <Routes>
+          <Route path="/home" element={<Home importedData={importData}/>} />
+          <Route path="/" element={<Layout sendDataToParent={handleDataFromChildCMP} />}>
         </Route>
       </Routes>
     </BrowserRouter>
