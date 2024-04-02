35b9456a3fd436a1dcb3881f9217e6a0d689a946
kananmhn <159273393+kananmhn@users.noreply.github.com>
2024-04-02 22:15:34 +0530

    Update FontSize.js

--- src/components/FontSize/FontSize.js
+++ src/components/FontSize/FontSize.js
@@ -1,16 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 
 
-function FontSize({sendDataToParent}) {
+function FontSize({sendDataToParent,importdata}) {
     const [FontSize, setFontSize] = useState("16px");
     const handleBackgroundColorChange = (event) => {
       setFontSize(event.target.value);  
       sendDataToParent(event.target.value);
       };
+      useEffect(() => {
+        if(importdata) {
+          setFontSize(importdata);
+        }
+      },[importdata]) 
     return (
         <form>
         <label>
-        <span className='lable-text'>Font Size:</span>
+        <span className='lable-text'>Primary Font Size:</span>
           <div className='color-box'><input type="input" value={FontSize} onChange={handleBackgroundColorChange} />
         </div></label>
         {/* <input type="submit" value="Submit" /> */}
