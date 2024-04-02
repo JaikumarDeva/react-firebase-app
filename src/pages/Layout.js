b61e24037451fb5e33aa17bc15933829dafbac60
kananmhn <159273393+kananmhn@users.noreply.github.com>
2024-04-02 22:32:44 +0530

    Update Layout.js

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,15 +1,14 @@
-import React, { useRef } from "react";
+import React, { useState,useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import "../App.css";
 import { Modal } from 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import logo from './logo-1.jpg';
 
-function Layout() {
 
+function Layout({sendDataToParent}) {
     const navigate = useNavigate();
     const fileInput = useRef();
-
     const handleImportClick = () => {
         const modalElement = document.getElementById('uploadModal');
         const bsModal = new Modal(modalElement);
@@ -22,33 +21,55 @@ function Layout() {
         myModalE2.remove();
         myModalEl.setAttribute('aria-hidden', 'true');
         myModalEl.setAttribute('style', 'display: none');
+        let body = document.querySelector('body');
+        body.classList.remove('modal-open');
+        body.classList.add('remove-hidden');
     }
-    const handleFileUpload = () => {
+    const handleFileUpload = (e) => {
         const file = fileInput.current.files[0];
-        if (file) {
-            const reader = new FileReader();
-            reader.onload = (event) => {
-                const data = JSON.parse(event.target.result);
-                console.log(data);
-                // Assuming the JSON data is an array and you want to store the first item
-                const item = data[0];
-                localStorage.setItem('color_id', item.color_id);
-                localStorage.setItem('fColor', item.fontColor);
-                localStorage.setItem('BGColor', item.backgroundColor);
-                localStorage.setItem('BtnTextColor1', item.BtnTextColor);
-                localStorage.setItem('BtnbgColor', item.BtnBgcolor);
-                localStorage.setItem('fontSize', item.fontSize);
-            };
-            reader.readAsText(file);
-        }
+        // if (file) {
+        //     const reader = new FileReader();
+        //     reader.onload = (event) => {
+        //         const data = JSON.parse(event.target.result);
+        //         console.log(data);
+        //         // Assuming the JSON data is an array and you want to store the first item
+        //         const item = data[0];
+        //         localStorage.setItem('color_id', item.color_id);
+        //         localStorage.setItem('fColor', item.fontColor);
+        //         localStorage.setItem('BGColor', item.backgroundColor);
+        //         localStorage.setItem('BtnTextColor1', item.BtnTextColor);
+        //         localStorage.setItem('BtnbgColor', item.BtnBgcolor);
+        //         localStorage.setItem('fontSize', item.fontSize);
+        //     };
+        //     reader.readAsText(file);
+        // }
+        // Handle the file upload here
+       // console.log(file);
         const modalElement = document.getElementById('uploadModal');
         const bsModal = new Modal(modalElement);
         bsModal.hide();
+        var reader = new FileReader();
+        var fileToRead = document.querySelector('input').files[0];
+        reader.addEventListener("loadend", function() {
+       // alert(reader.result);
+        sendDataToParent(reader.result);
+        });
+        reader.readAsText(fileToRead);
+        let myModalEl = document.getElementById('uploadModal');
+        let myModalE2 = document.querySelector('.modal-backdrop');
+        myModalEl.classList.remove('show');
+        myModalE2.remove();
+        let body = document.querySelector('body');
+        body.classList.remove('modal-open');
+        body.classList.add('remove-hidden')
+        myModalEl.setAttribute('aria-hidden', 'true');
+        myModalEl.setAttribute('style', 'display: none');
+        navigate("/home")
     };
-
+    
   return (
     <div className="App mt-5">
-            <div className="teamLogo">
+       <div className="teamLogo">
                 {/*  eslint-disable-next-line */}
                 <img src={logo} className="img-fluid" />
             </div>
@@ -60,7 +81,6 @@ function Layout() {
             <div onClick={() => navigate("/home")} className="select_base_theme">
                 Select Base Theme
             </div>
-           
             <div onClick={handleImportClick} className="select_base_theme">
                import a Theme
             </div>
@@ -76,7 +96,7 @@ function Layout() {
                                         </button>
                                     </div>
                                     <div className="modal-body">
-                                        <input type="file" ref={fileInput} />
+                                        <input accept=".json" type="file" ref={fileInput} />
                                     </div>
                                     <div className="modal-footer">
                                         <button type="button" onClick={handleCloseModel}  className="btn btn-secondary" data-dismiss="modal">Close</button>
